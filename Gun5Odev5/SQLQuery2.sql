11abccefc4a9b4308477878869d29fbdd3b31d9e
asli2754 <asliefe2754@gmail.com>
2021-02-07 23:48:38 +0300

    gun7

---
+++ Gun5Odev5/SQLQuery2.sql
@@ -0,0 +1,26 @@
+﻿select * from Products
+
+SELECT Products.ProductName AS UrunAdi,
+SUM([Order Details].UnitPrice*[Order Details].Quantity) AS KazanilanToplamMiktar
+from Products inner join [Order Details] on Products.ProductID = [Order Details].ProductID 
+  inner join Orders
+on Orders.OrderID = [Order Details].OrderID
+group by ProductName
+order by KazanilanToplamMiktar desc;
+
+SELECT Products.ProductName AS Product  , 
+SUM([Order Details].Quantity*Products.UnitPrice) AS TotalEarning 
+FROM [Order Details] INNER JOIN Products
+ON Products.ProductID=[Order Details].ProductID 
+GROUP BY Products.ProductName
+order by TotalEarning desc;
+
+select ProductName as UrunAdi, sum((od.UnitPrice)*(od.Quantity)) as KazanilanToplamMiktar
+from Products as p inner join [Order Details]  as od 
+
+on  p.ProductID=od.ProductID
+
+inner join Orders as o on od.OrderID=o.OrderID
+
+group by ProductName
+order by ProductName asc;
---
+++ MyFinalProject/Business/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    //iş katmanında kullanacağımız servis operasyonları
+    public interface IProductService
+    {
+        List<Product> GetAll();
+    }
+}
