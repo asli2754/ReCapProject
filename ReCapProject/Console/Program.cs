047fc35e2cbf7c6ef5d73f3b4e78c465e2b6960f
asli2754 <asliefe2754@gmail.com>
2021-02-08 23:52:54 +0300

    ReCapProject

---
+++ ReCapProject/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Car car1 = new Car() { Description = "Opel", BrandID = 5, ColorID = 31, DailyPrice = 120000, ID = 4, ModelYear = new DateTime(2012) };
+            carManager.Add(car1);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear + " Model " + car.Description + " : " + car.DailyPrice + "TL");
+            }
+            
+
+        }
+    }
+}
---
+++ ReCapProject/Entities/Concrete/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Car:IEntity
+    {
+        public int ID { get; set; }
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public DateTime ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+
+    }
+}
